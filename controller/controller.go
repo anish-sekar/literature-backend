e535b880aba7c43dd6b9a699519ea0e5d170db55
Anish <anish.sekar94@gmail.com>
2020-08-20 23:29:37 -0700

    Boilerplate & basic logic

---
+++ controller/controller.go
@@ -0,0 +1,67 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"github.com/anish-sekar/literature-backend/utils"
+)
+// Controller example
+type Games struct {
+
+Room map[string] Match
+
+}
+
+type Match struct{
+
+ID string
+P1 Player `json:"P1"`
+P2 Player `json:"P2"`
+P3 Player `json:"P3"`
+P4 Player `json:"P4"`
+P5 Player `json:"P5"`
+P6 Player `json:"P6"`
+
+}
+
+type Player struct {
+	
+	UserName string `json:"username"`
+	Cards []string `json:"cards"`
+	SocketConnection *websocket.Conn
+
+}
+
+// NewController example
+func NewController() *Games {
+	return &Games{}
+}
+
+func (games *Games) CreateGame(ctx *gin.Context){
+
+	game_code := utils.GenerateGameCode(4)
+	match := games.Room[game_code]
+	match.ID = game_code
+	
+	//Generating the cards for the game
+	suits :=[...]string{"S", "D", "C", "H"}
+	values :=[...]string{"A", "2", "3", "4", "5", "6", "7", "9", "10", "J", "Q", "K"}
+
+	var deck [] string
+
+	for _, suit := range suits {
+		for _, value := range values {
+			deck = append(deck, (suit+value))
+		}
+	}
+	deck= utils.Shuffle(deck)
+	
+	match.P1.Cards=deck[0:7]
+	match.P2.Cards=deck[8:15]
+	match.P3.Cards=deck[16:23]
+	match.P4.Cards=deck[24:31]
+	match.P5.Cards=deck[32:39]
+	match.P6.Cards=deck[40:47]
+
+
+}
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 
 	"github.com/anish-sekar/literature-backend/utils"
-	"github.com/anish-sekar/literature-backend/models"
+	//"github.com/anish-sekar/literature-backend/models"
 	"flag"
 	//"html/template"
 	"log"
@@ -18,11 +18,20 @@ import (
 )
 
 
-var activeConnections []websocket.Conn
-var abs_state models.AbsoluteState
+type Game struct{
 
-func main() {
+	ActiveConnections []*websocket.Conn
 
+}
+
+//var abs_state models.AbsoluteState
+
+func smain() {
+
+
+
+
+gameObject := Game{}
 
 //Generating the cards for the game
 suits :=[...]string{"S", "D", "C", "H"}
@@ -50,37 +59,26 @@ for _, player:= range players{
 		fmt.Println(player)
 }
 
-
 ret := utils.GenerateGameCode(10)
 fmt.Println(ret)
 
 
 
-
-// err := json.Unmarshal([]byte(s), &abs_state)
-// if err != nil {
-// 	log.Println("read:", err)
-// }
-
-// fmt.Println(abs_state.State.P1)
-
-
-
-http.HandleFunc("/", echo)
+http.HandleFunc("/", gameObject.connect)
 log.Fatal(http.ListenAndServe(*addr, nil))
 
 }
 
 //var abs_state models.AbsoluteState
 
-var addr = flag.String("addr", "localhost:8081", "http service address")
+var addr = flag.String("addr", "localhost:5000", "http service address")
 
 var upgrader = websocket.Upgrader{	CheckOrigin: func(r *http.Request) bool {
 	return true
 },} 
 
 
-func echo(w http.ResponseWriter, r *http.Request) {
+func (game *Game) connect(w http.ResponseWriter, r *http.Request) {
 	
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -88,7 +86,7 @@ func echo(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	activeConnections = append(activeConnections,*c)
+	game.ActiveConnections = append(game.ActiveConnections,c)
 
 	defer c.Close()
 	//c.SetCloseHandler(printCloseMessage)
@@ -98,62 +96,25 @@ func echo(w http.ResponseWriter, r *http.Request) {
 
 	for {
 
-		c.ReadJSON(&abs_state)
+		var i map[string]interface{}
+		c.ReadJSON(&i)
 		if err != nil {
 			log.Println("read:", err)
 			break
 		}
-		shoutOut(activeConnections)
-		fmt.Println(abs_state)
-		
 
-		// mt, message, err := c.ReadMessage()
-		// if err != nil {
-		// 	log.Println("read:", err)
-		// 	break
-		// }
 
-		// log.Printf("recv: %s", message)
-		// err = c.WriteMessage(mt, message)
-		// if err != nil {
-		// 	log.Println("write:", err)
-		// 	break
-		// }
+		shoutOut(game.ActiveConnections)
+
 	}
+
 }
 
 
-func shoutOut(c []websocket.Conn){
+func shoutOut(c []*websocket.Conn){
 
 	s := `{
-		"id": "XVlBzgbaiC",
-		"turn": "anish#2345",
-		"state": {
-		  "P1": {
-			"username": "a",
-			"cards": ["H4","SA"]
-		  },
-		  "P2": {
-			"username": "b",
-			"cards": ["H4","SA"]
-		  },
-		  "P3": {
-			"username": "c",
-			"cards": ["H4","SA"]
-		  },
-		  "P4": {
-			"username": "d",
-			"cards": ["H4","SA"]
-		  },
-		  "P5": {
-			"username": "e",
-			"cards": ["H4","SA"]
-		  },
-		  "P6": {
-			"username": "e",
-			"cards": ["H4","SA"]
-		  }
-		}
+		"hello":"padowan"
 	  }`
 for _,con:= range c{
 
