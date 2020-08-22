e8e435c99335678b86f250e814088d6f6a3861a4
Anish <anish.sekar94@gmail.com>
2020-08-22 12:40:45 -0700

    create,get games and join functionality

--- controller/controller.go
+++ controller/controller.go
@@ -4,23 +4,21 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 	"github.com/anish-sekar/literature-backend/utils"
+	"github.com/anish-sekar/literature-backend/models"
 )
 // Controller example
-type Games struct {
+type World struct {
 
-Room map[string] Match
+Games map[string]*Match
 
 }
 
 type Match struct{
 
-ID string
-P1 Player `json:"P1"`
-P2 Player `json:"P2"`
-P3 Player `json:"P3"`
-P4 Player `json:"P4"`
-P5 Player `json:"P5"`
-P6 Player `json:"P6"`
+CurrentTurn int
+Players []*Player 
+Team1 []int 
+Team2 []int
 
 }
 
@@ -33,34 +31,86 @@ type Player struct {
 }
 
 // NewController example
-func NewController() *Games {
-	return &Games{}
+func NewController() *World {
+	return &World{}
 }
 
-func (games *Games) CreateGame(ctx *gin.Context){
+func (world *World) StartGame(ctx *gin.Context){ 
+
+	
+
+	}
+
+func (world *World) GetGames(ctx *gin.Context){
+
+
+	ctx.JSON(200, gin.H{
+		"status":  "success",
+		"details":world,
+	})
+
+
+}
+
+
+
+func (world *World) CreateGame(ctx *gin.Context){
 
 	game_code := utils.GenerateGameCode(4)
-	match := games.Room[game_code]
-	match.ID = game_code
+	world.Games[game_code] = &Match{}
 	
-	//Generating the cards for the game
-	suits :=[...]string{"S", "D", "C", "H"}
-	values :=[...]string{"A", "2", "3", "4", "5", "6", "7", "9", "10", "J", "Q", "K"}
+	
+	ctx.JSON(200, gin.H{
+		"status":  "success",
+		"game_code":game_code,
+		"details":world.Games[game_code],
+	})
 
-	var deck [] string
+	
+	// //Generating the cards for the game
+	// suits :=[...]string{"S", "D", "C", "H"}
+	// values :=[...]string{"A", "2", "3", "4", "5", "6", "7", "9", "10", "J", "Q", "K"}
 
-	for _, suit := range suits {
-		for _, value := range values {
-			deck = append(deck, (suit+value))
-		}
+	// var deck [] string
+
+	// for _, suit := range suits {
+	// 	for _, value := range values {
+	// 		deck = append(deck, (suit+value))
+	// 	}
+	// }
+	// deck= utils.Shuffle(deck)
+
+	// match.P1.Cards=deck[0:7]
+	// match.P2.Cards=deck[8:15]
+	// match.P3.Cards=deck[16:23]
+	// match.P4.Cards=deck[24:31]
+	// match.P5.Cards=deck[32:39]
+	// match.P6.Cards=deck[40:47]
+
+}
+
+func (world *World) JoinGame(ctx *gin.Context){
+
+	//Bind request body to i
+	var i models.JoinGamePayload
+	ctx.BindJSON(&i)
+
+		
+	if world.Games[i.GameCode].Players == nil{
+
+		world.Games[i.GameCode].Players = make([]*Player,0)
+
 	}
-	deck= utils.Shuffle(deck)
+	
+	//Use i to add user to the game
+	world.Games[i.GameCode].Players  = append(world.Games[i.GameCode].Players,&Player{ UserName:i.UserName})
 
-	match.P1.Cards=deck[0:7]
-	match.P2.Cards=deck[8:15]
-	match.P3.Cards=deck[16:23]
-	match.P4.Cards=deck[24:31]
-	match.P5.Cards=deck[32:39]
-	match.P6.Cards=deck[40:47]
+
+	ctx.JSON(200, gin.H{
+		"status":  "success",
+		"username":i.UserName,
+	})
+
+
 
 }
--- main.go
+++ main.go
@@ -1,126 +1,26 @@
 package main
 
 import (
-
-	"github.com/anish-sekar/literature-backend/utils"
-	//"github.com/anish-sekar/literature-backend/models"
-	"flag"
-	//"html/template"
-	"log"
-	"net/http"
-    //"errors"
-    "fmt"
-    //"math/rand"
-	//"time"
-	//"reflect"
-	//"encoding/json"
-	"github.com/gorilla/websocket"
+	"github.com/gin-gonic/gin"
+	//"github.com/gorilla/websocket"
+	"github.com/anish-sekar/literature-backend/controller"
+	"fmt"
 )
 
+func main() {
 
-type Game struct{
-
-	ActiveConnections []*websocket.Conn
-
-}
-
-//var abs_state models.AbsoluteState
-
-func smain() {
-
-
-
-
-gameObject := Game{}
-
-//Generating the cards for the game
-suits :=[...]string{"S", "D", "C", "H"}
-values :=[...]string{"A", "2", "3", "4", "5", "6", "7", "9", "10", "J", "Q", "K"}
-
-var deck [] string
-
-for _, suit := range suits {
-    for _, value := range values {
-        deck = append(deck, (suit+value))
-    }
-}
-deck= utils.Shuffle(deck)
-
-var players [][] string 
-
-players = append(players,deck[0:7])
-players = append(players,deck[8:15])
-players = append(players,deck[16:23])
-players = append(players,deck[24:31])
-players = append(players,deck[32:39])
-players = append(players,deck[40:47])
-
-for _, player:= range players{
-		fmt.Println(player)
-}
-
-ret := utils.GenerateGameCode(10)
-fmt.Println(ret)
-
-
-
-http.HandleFunc("/", gameObject.connect)
-log.Fatal(http.ListenAndServe(*addr, nil))
-
-}
-
-//var abs_state models.AbsoluteState
-
-var addr = flag.String("addr", "localhost:5000", "http service address")
-
-var upgrader = websocket.Upgrader{	CheckOrigin: func(r *http.Request) bool {
-	return true
-},} 
-
-
-func (game *Game) connect(w http.ResponseWriter, r *http.Request) {
+	w:= controller.NewController()
+	w.Games= make(map[string]*controller.Match)
 	
-	c, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Print("upgrade:", err)
-		return
-	}
 
-	game.ActiveConnections = append(game.ActiveConnections,c)
-
-	defer c.Close()
-	//c.SetCloseHandler(printCloseMessage)
-	log.Print("JUST CONNECTED:")
-	log.Println(c.RemoteAddr())
+	fmt.Print(w)
+	r := gin.Default()
 
 
-	for {
+	r.GET("/create", w.CreateGame)
+	r.GET("/games",w.GetGames)
+	r.POST("start".w.StartGame)
+	r.PUT("/join",w.JoinGame)
 
-		var i map[string]interface{}
-		c.ReadJSON(&i)
-		if err != nil {
-			log.Println("read:", err)
-			break
-		}
-
-
-		shoutOut(game.ActiveConnections)
-
-	}
-
+	r.Run(":5000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
-
-
-func shoutOut(c []*websocket.Conn){
-
-	s := `{
-		"hello":"padowan"
-	  }`
-for _,con:= range c{
-
-	con.WriteMessage(1,[]byte(s) )
-
-}
-
-}
-
